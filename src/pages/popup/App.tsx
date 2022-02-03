ba54f957a76af75f59cfafe9e8b04034a985c329
Dennis Dang <dangggdennis@gmail.com>
2022-02-03 13:04:24 -0500

    initial commit

---
+++ src/pages/popup/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Popup Page</h1>
+      <p>If you are seeing this, React is working!</p>
+      <p>Whatssssuup</p>
+    </div>
+  )
+}
+
+export default App
