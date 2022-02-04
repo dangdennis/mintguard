ef61e5834b49099da6badce6b5422e7b199352fa
Dennis Dang <dangggdennis@gmail.com>
2022-02-03 23:36:48 -0500

    add missing 8080 to fly toml env

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import { db } from "./database";
 
 const PORT = Number(process.env.PORT || 3000);
 
-app.listen(PORT, "0.0.0.0", async () => {
+app.listen(PORT, async () => {
 	await db.connect();
 	if (process.env.NODE_ENV !== "production") {
 		console.log(`server listening on http://localhost:3000/`);
