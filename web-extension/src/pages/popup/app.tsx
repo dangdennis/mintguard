bb8526fa5d25927a84813c3618279ae3503cbe15
Dennis Dang <dangggdennis@gmail.com>
2022-02-04 01:53:53 -0500

    update text, setup covalent api

--- web-extension/src/pages/popup/app.tsx
+++ web-extension/src/pages/popup/app.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 import { Toggle } from './toggle'
+// import axios from 'axios'
+
+// const covalent = axios.create({
+//   baseURL: 'https://api.covalenthq.com/v1/1',
+//   params: {
+//     format: 'JSON',
+//     key: 'ckey_a6cac550414b4197863a8ebd7d7',
+//   },
+// })
 
 export const App = (): JSX.Element => {
   const [enabled, setEnabled] = useState(false)
 
   return (
-    <div className="w-48 h-40 p-2 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="w-48 h-48 p-2 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h1 className="text-lg font-bold text-white">Minting Protection</h1>
       <div
         className="grid w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
@@ -20,7 +29,8 @@ export const App = (): JSX.Element => {
         {enabled && (
           <div className="mt-4">
             <p className="text-white">
-              We&apos;ll make sure your minting experience is safe.
+              We&apos;ll alert you if you&apos;re minting on a risky
+              site or an unverified NFT project.
             </p>
           </div>
         )}
